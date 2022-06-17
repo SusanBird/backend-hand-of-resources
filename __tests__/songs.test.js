e37b32e89dd386438144a8bd94a978ec582ea5ea
SusanBird <susanbird391@gmail.com>
2022-06-17 15:29:02 -0700

    list of songs test written

---
+++ __tests__/songs.test.js
@@ -0,0 +1,24 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const Songs = require('../lib/models/Songs');
+
+describe('song routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('/songs should return a list of songs', async () => {
+    const res = await request(app).get('/songs');
+    const songsData = Songs.getAll();
+    const expected = (await songsData).map((song) => {
+      return { name: song.name, age: song.age, iq: song.iq, energy: song.energy };
+    });
+    expect(res.body).toEqual(expected);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
--- sql/setup.sql
+++ sql/setup.sql
@@ -3,6 +3,9 @@
 
 DROP table if exists dogs;
 DROP table if exists shows;
+DROP table if exists songs;
+DROP table if exists games;
+DROP table if exists food;
 
 CREATE table dogs (
     id BIGINT GENERATED ALWAYS AS IDENTITY,
@@ -19,6 +22,27 @@ CREATE table shows (
     rating INT NOT NULL
 );
 
+CREATE table songs (
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    title VARCHAR NOT NULL,
+    artist VARCHAR NOT NULL,
+    released INT NOT NULL
+);
+
+CREATE table games (
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR NOT NULL,
+    players INT NOT NULL,
+    difficulty INT NOT NULL
+);
+
+CREATE table food (
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR NOT NULL,
+    country VARCHAR NOT NULL,
+    group VARCHAR NOT NULL
+);
+
 INSERT INTO dogs (name, age, iq, energy) VALUES
 ('Chance', 7, 60, 'high'),
 ('Tate', 2, 115, 'medium'),
@@ -30,3 +54,21 @@ INSERT INTO shows (title, seasons, rating) VALUES
 ('Tayo', 7, 3),
 ('The Great British Baking Show', 8, 10),
 ('The Office', 11, 10);
+
+INSERT INTO songs (title, artist, released) VALUES
+('Never Gonna Give You Up', 'Rick Astley', 1880),
+('Cruise', 'Some Country Band', 2015),
+('Baby Shark', 'Someone Without A Soul', 2020),
+('Gangam Style', 'Mr. Gangham', 2012);
+
+INSERT INTO games (name, players, difficulty) VALUES
+('Cover Your Assets', 5, 3),
+('Monopoly', 4, 6),
+('Twister', 2, 10),
+('Sorry', 4, 3);
+
+INSERT INTO food (name, country, group) VALUES
+('bananas', 'Brazil', 'fruit'),
+('tacos', 'Mexico', 'meat'),
+('roti', 'Fiji', 'grains'),
+('lumpia', 'Philippines', 'vegetables');
